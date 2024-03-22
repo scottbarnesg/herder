25edc6a35e981fcfccd3903594f5be56d308dd5e
Scott Barnes <sgbarnes@protonmail.com>
2024-03-21 20:29:23 -0500

    Fix WorkDir path in StartProject

--- commands/run.go
+++ commands/run.go
@@ -13,10 +13,11 @@ func StartProject(projectName string, config *config.Config) error {
 	}
 	log.Printf("Starting services for project %s...\n", projectName)
 	for i, service := range project.Services {
+		workDir := utils.GetFullPath(project.Path, service.Path)
 		log.Printf("\t%d. Starting service %s...\n", i+1, service.Name)
 		log.Printf("\t\tCommand: %s\n", service.BuildCommand)
-		log.Printf("\t\tDirectory: %s\n", service.Path)
-		out, err := utils.RunCommand(service.Path, service.RunCommand)
+		log.Printf("\t\tDirectory: %s\n", workDir)
+		out, err := utils.RunCommand(workDir, service.RunCommand)
 		if err != nil {
 			return err
 		}
